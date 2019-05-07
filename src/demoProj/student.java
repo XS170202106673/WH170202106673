57855c753250e0d70088e3fcb9229740ffdfb4b6
Administrator <Administrator@MB404-94>
2019-05-07 14:37:11 +0800

    fine

---
+++ src/demoProj/student.java
@@ -0,0 +1,18 @@
+package demoProj;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		String address; 
+		student();
+
+	}
+
+	private static void student() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
